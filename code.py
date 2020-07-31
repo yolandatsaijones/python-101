297e1bd9289c813d684fb6cd3f0634536df72a26
yolandatsaijones <69053222+yolandatsaijones@users.noreply.github.com>
2020-07-31 14:49:31 -0700

    Add files via upload

---
+++ code.py
@@ -0,0 +1,13 @@
+#This is a comment
+print("Hello world!")
+egg = raw_input('What is your name?')
+#naming it egg just to prove that you can name a varaiable anything
+print("it's good to meet you" + egg)
+lamb = len(egg)
+print ('the length of your name is')
+print(lamb)
+number = int(raw_input ('what is your age?'))
+#raw_input is a string so i have to cast it into a interger
+happy = 1
+print('you will be ' + str(number+happy) +' in a year')
+#number+happy is a interger so i have to cast it into string
